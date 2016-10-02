586621acb6f5759e0f928d8cdbf4021f16b6d2e7
Glen Aultman-Bettridge <gaultmanbettridge@fluorofinder.com>
2016-10-01 20:56:57 -0600

    Adds a barebones list of favorites and favoriting books functionality. Needs a oodles of cleanup and much better UX.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,8 @@ class BooksController < ApplicationController
   def search
     @search_page = Book.search(book_params[:search_term])
 
+    @favorited_collection = favorited
+
     render :index
   end
 
@@ -15,10 +17,19 @@ class BooksController < ApplicationController
   end
 
   def update
+    book = Book.find_or_create_by(book_params)
+    book.add_favorite
+    book.save!
+
+    @favorited_collection = favorited
   end
 
   private
   def book_params
-    params.require(:book).permit(:search_term, :id, :favorite)
+    params.require(:book).permit(:search_term, :google_book_id, :thumbnail_link, :title, :authors)
+  end
+
+  def favorited
+    Book.all.order('favorited_count desc')
   end
 end
